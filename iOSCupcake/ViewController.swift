e9337c3cf8a9d3222258380a91aa1e69c32e1931
Hassan Abid <hassanabidpk89@gmail.com>
2016-08-07 15:46:32 +0900

    Initial Commit

---
+++ iOSCupcake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSCupcake
+//
+//  Created by Hassan Abid on 8/7/16.
+//  Copyright © 2016 Hassan Abid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
